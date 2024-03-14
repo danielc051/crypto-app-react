24b7c44572555b571ff01c3336beadbba54b0aa8
danielc051 <connerydaniel@gmail.com>
2024-03-15 08:54:44 +1300

    Started react project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <>
-    
-    
-    
-    </>
-  );
-}
+  const [coins, setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+        setCoins(response.data)
+        console.log(response.data[0])
+      }).catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
 
 export default App;
