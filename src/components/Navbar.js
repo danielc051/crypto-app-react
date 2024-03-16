d979f817eccb4d03134dd838262e938ad88e23ed
danielc051 <connerydaniel@gmail.com>
2024-03-16 17:38:26 +1300

    Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
-import React from "react";
-import {Facoins} from 'react-icons/fa'
+import React from 'react'
+import {FaCoins} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>
-      <div className="navbar">
-        <Facoins className='icon'/>
-        <h1>Coin <span className="purple">Search</span></h1>
-        </div>;
-    </div>
-  )
-};
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <FaCoins className='icon' />
+                <h1> Coin <span className='purple'>Search</span></h1>
+            </div>
+        </Link>
+    )
+}
 
-export default Navbar;
+export default Navbar
